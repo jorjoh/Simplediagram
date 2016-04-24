43236358a903a98716a948c483a3e419893ec1e8
Jørgen Johansen <post@jorgenjohansen.no>
2016-04-24 19:13:25 +0200

    fixes code

--- src/uml/model/ClassSymbol.java
+++ src/uml/model/ClassSymbol.java
@@ -21,25 +21,22 @@ public class ClassSymbol extends Figuremodel {
 		reference = new Point(500, 180);
 		Point arm = new Point(500, 220);
 		Point venstre = new Point(150, 700);
-		Point bunn = new Point(700, 700);
 		Point hoyreside = new Point(700, 100);
+
+		Point bunn = new Point(700, 700);
 		Point topp = new Point(700, 100);
 		virtual = new Dimension(200, 200);
 
-
 		// venstreside
 		elements.add(new LineElement(venstre, new Point(150, 100), Hold.END));
-		// bunn
-		elements.add(new LineElement(bunn, new Point(150, 700), Hold.NONE));
 		// hoyreside
 		elements.add(new LineElement(hoyreside, new Point(700, 700), Hold.END));
+
+		// bunn
+		elements.add(new LineElement(bunn, new Point(150, 700), Hold.NONE));
+
 		// topp
 		elements.add(new LineElement(topp, new Point(150, 100), Hold.NONE));
-
-		// venstre bein
-		//elements.add(new LineElement(side2, new Point(100, 700), Hold.NONE));
-		// h�yre bein
-		//elements.add(new LineElement(side2, new Point(100, 700), Hold.NONE));
 
 		elements.add(new TextElement("ClassSymbol", LabelAlign.BOTTOM));
 	}
